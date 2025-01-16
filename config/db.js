33c4238716699bb504b432d1b35dbd399fc6e6bb
Gowtham640 <grizigowtham@gmail.com>
2025-01-16 10:40:08 +0530

    Your commit message here

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     console.log('MongoDB connected');
   } catch (error) {
     console.error(error);
-    process.exit(1); // Exit the process if DB connection fails
+    process.exit(1); 
   }
 };
 
